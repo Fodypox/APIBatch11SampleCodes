d99b7f24bf1dc18914df60573e8ea548d94f815a
aslanbekturganbayev <aslanbeksmailbox@gmail.com>
2024-05-22 13:25:07 -0400

    first commit

---
+++ src/test/java/POJO_Models/StatePOJO.java
@@ -0,0 +1,49 @@
+package POJO_Models;
+
+public class StatePOJO {
+    private String id;
+    private String name;
+    private String shortName;
+    private CountryPOJO country;
+    private String[] translateName = {};
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
+    public CountryPOJO getCountry() {
+        return country;
+    }
+
+    public void setCountry(CountryPOJO country) {
+        this.country = country;
+    }
+
+    public String[] getTranslateName() {
+        return translateName;
+    }
+
+    public void setTranslateName(String[] translateName) {
+        this.translateName = translateName;
+    }
+}
